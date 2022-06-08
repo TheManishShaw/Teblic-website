91321334871f04c17c38c1c78805b7c9509eef69
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-06-08 17:45:34 +0530

    error fiexed

--- pages/blogs/SingleBlog.jsx
+++ pages/blogs/SingleBlog.jsx
@@ -5,13 +5,15 @@ const SingleBlog = (props) => {
   return (
     <div className=" rounded-xl shadow-xl bg-white hover:scale-105 hover:ease-in-out transition-all">
       <Link href="/blogs/Blog-2">
-        <Image
-          src={props.img}
-          alt="Blog Images"
-          width={1000}
-          height={400}
-          className="rounded-t-lg h-60 w-full object-cover cursor-pointer"
-        />
+        <a>
+          <Image
+            src={props.img}
+            alt="Blog Images"
+            width={1000}
+            height={400}
+            className="rounded-t-lg h-60 w-full object-cover cursor-pointer"
+          />
+        </a>
       </Link>
       <p className="mt-3 px-9 text-xs font-semibold tracking-wide uppercase">
         <a
@@ -23,10 +25,12 @@ const SingleBlog = (props) => {
         <span className="text-gray-600">— {props.date}</span>
       </p>
       <Link href="/blogs/Blog-2">
-        <header className=" text-xl font-bold p-3 px-9">{props.title}</header>
-        <div className="px-5">
-          <p className=" px-4 text-zinc-500">{props.desc}</p>
-        </div>
+        <a>
+          <header className=" text-xl font-bold p-3 px-9">{props.title}</header>
+          <div className="px-5">
+            <p className=" px-4 text-zinc-500">{props.desc}</p>
+          </div>
+        </a>
       </Link>
       <footer className="text-left  py-2 px-5 text-gray-500 mb-2">
         <div className="flex items-center  inset-x-0 bottom-0 flex-wrap mt-2">
