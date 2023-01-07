1e312533ee40e7c4633ad1e32c589d5c9f486f53
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2023-01-07 13:51:22 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import Head from 'next/dist/shared/lib/head';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <meta name="description" content="Tecblic Website"/>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4763056878068003"
+     crossorigin="anonymous"></script>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
